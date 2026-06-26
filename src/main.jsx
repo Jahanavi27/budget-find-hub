e62ea517eb2da7d4e91d2f0065c78fee7b8e2e50
Jahanavi27 <Jahanavi27>
2026-06-26 06:37:50 +0530

    Added Google Analytics GA4

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { initGA, trackPageView } from './analytics'
+
+initGA();
+trackPageView(window.location.pathname);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-
-
